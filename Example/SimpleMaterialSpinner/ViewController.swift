43878be7d349ea32ccde012893c22515ea5e1e1e
Ilya Kharlamov <ilia3546@me.com>
2018-05-21 19:08:52 +0300

    Initial commit

---
+++ Example/SimpleMaterialSpinner/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SimpleMaterialSpinner
+//
+//  Created by ilia3546@me.com on 05/21/2018.
+//  Copyright (c) 2018 ilia3546@me.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
